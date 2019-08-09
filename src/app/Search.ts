2b370150015efab141dd9200d26382e6fad809a0
nehabaghele <nehabaghele643@gmail.com>
2019-08-09 19:49:46 +0530

    Initial commit

---
+++ src/app/Search.ts
@@ -0,0 +1,6 @@
+// this is the data structure for the Seach class (body type)
+export class Search{
+	SearchElement: string;
+	val: Array<Object>;
+	SearchTime: string;
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SearchService } from './search.service';
+import { SearchComponent } from './search/search.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'search';
+}
